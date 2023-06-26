2accd1d14eba3afa7cf845d921e9941477a80239
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-06-26 16:32:15 +0300

    try/catch and create error notification

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,3 @@
-
 <template>
   <main class="container text-white">
     <div class="pt-4 mb-8 relative">
@@ -11,14 +10,23 @@
       />
       <ul
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
       >
-        <li
-          v-for="searchResult in mapboxSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ searchResult.place_name }}
-        </li>
+        <p class="py-2" v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -31,21 +39,28 @@ import axios from "axios";
 const mapboxAPIKey =
   "pk.eyJ1IjoidmJvbmRhciIsImEiOiJjbGpjdHUwemEwNXJrM2pzMmVweHZmaGZyIn0.ba08N3pplLUsHng3Q-AjQA";
 const searchQuery = ref("");
-const queryTimout = ref(null);
+const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
-  clearTimeout(queryTimout.value);
-  queryTimout.value = setTimeout(async () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapboxSearchResults.value = result.data.features;
-      console.log("mapboxSearchResults.value", mapboxSearchResults.value);
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
+
       return;
     }
     mapboxSearchResults.value = null;
   }, 300);
 };
 </script>
+
+<style lang="scss" scoped></style>
