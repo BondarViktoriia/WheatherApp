9c7eadb1b9f1055587e94a851e0457728b32870a
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-06-27 16:31:58 +0300

    cor key api weather

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -10,7 +10,7 @@ const route = useRoute();
 const getWeatherData = async () => {
   try {
     const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=20f56119476596c34637a844ad857552&units=imperial`
     );
 
     // cal current date & time
